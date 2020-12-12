5329e607ddc80d835734eea0cc077f1b05592b44
yangahh <toaur6802@naver.com>
2020-12-12 09:54:41 +0900

    my web page

---
+++ main.js
@@ -0,0 +1,8 @@
+const navMenuBtn = document.querySelector('.nav-btn-item');
+const navMenuList = document.querySelector('.navbar-menu');
+
+// 메뉴 버튼이 클릭될 때마다 이벤트 처리
+navMenuBtn.addEventListener('click', () => {
+    // 클릭했을 때 active가 있다면 넣어주고 없으면 빼기
+    navMenuList.classList.toggle('active');
+});
