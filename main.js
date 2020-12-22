9ddef2c3b541116ab0ceb646c6f06bdfe634f67f
yangahh <toaur6802@naver.com>
2020-12-23 03:35:25 +0900

    add typing game html css

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const navMenuBtn = document.querySelector('.nav-btn-item');
-const navMenuList = document.querySelector('.navbar-menu');
+"user strict";
+
+// Navigation
+const navMenuBtn = document.querySelector(".nav-btn-item");
+const navMenuList = document.querySelector(".navbar-menu");
 
 // 메뉴 버튼이 클릭될 때마다 이벤트 처리
-navMenuBtn.addEventListener('click', () => {
-    // 클릭했을 때 active가 있다면 넣어주고 없으면 빼기
-    navMenuList.classList.toggle('active');
+navMenuBtn.addEventListener("click", () => {
+  // 클릭했을 때 active가 있다면 넣어주고 없으면 빼기
+  navMenuList.classList.toggle("active");
 });
+
+// Game page
+// input box에 글자 입력하면 값 받아오기
